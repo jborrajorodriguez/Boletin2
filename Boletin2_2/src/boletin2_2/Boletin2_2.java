5a4da2d5f8bae2b77ece69dc5425f18eeb82d274
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2017-10-26 09:32:16 +0200

    Boletin2_2

---
+++ Boletin2_2/src/boletin2_2/Boletin2_2.java
@@ -0,0 +1,16 @@
+
+package boletin2_2;
+
+
+public class Boletin2_2 {
+    
+    public static void main(String[] args) {
+        
+        float lado=3f,area;
+        area=lado*lado;
+        System.out.println("Area ="+area);
+      
+        
+    }
+    
+}
